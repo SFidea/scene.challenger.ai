d4bb1ef4b893f607b2062ea11c73c7bfaec65b11
suzg@syber <suzg+syber@suzg.net>
2017-09-06 10:59:01 +0800

    use files in original

--- json2yml.py
+++ json2yml.py
@@ -3,11 +3,7 @@ import json
 import yaml
 
 
-if len(sys.argv)!=2:
-    print "Usage: python %s <json file>" % sys.argv[0]
-    sys.exit(1)
-
-with open(sys.argv[1], 'rb') as f:
+with open('original/scene_train_annotations.json', 'rb') as f:
     annotations = json.load(f)
 
 print yaml.safe_dump(annotations, default_flow_style=False, allow_unicode=True)
